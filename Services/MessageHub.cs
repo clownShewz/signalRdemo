220178deff50fdce70b53aad605239bd3265e520
clownShewz <tmgllghr@gmail.com>
2018-03-25 20:18:38 -0400

    inititial check in

---
+++ Services/MessageHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemoHub.Services
+{
+    public class MessageHub : Hub
+    {
+
+        public  Task SendMessage(string message)
+        {
+            string timestamp = DateTime.UtcNow.ToString();
+            Console.WriteLine($"MessageHub Send Message: {message} at: {timestamp}");
+            return Clients.All.SendAsync("SendMessage", new { message, timestamp });
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.SignalR;
+using SignalRDemoHub.Services;
+
+namespace SignalRDemoHub
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => {
+                options.AddPolicy("CorsDevPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                           );
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory log)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseCors("CorsDevPolicy");
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<MessageHub>("/messages");
+            });
+
+
+            app.UseMvc();
+           
+
+     
+
+            log.AddConsole();
+        }
+    }
+}
